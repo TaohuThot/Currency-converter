fff257ad1c1fa8e67d800c069dcd2c7ac7204532
TaohuThot <third.nk20@gmail.com>
2024-10-07 23:41:13 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+// App.js
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // การประกาศสถานะ (State Variables) 
+    const [info, setInfo] = useState([]);
+    const [input, setInput] = useState(0);
+    const [from, setFrom] = useState("usd");
+    const [to, setTo] = useState("thb");
+    const [options, setOptions] = useState([]);
+    const [output, setOutput] = useState(0);
+
+    // การดึงข้อมูลจาก API เมื่อสกุลเงิน
+    useEffect(() => {
+        Axios.get(
+`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`)
+            .then((res) => {
+                setInfo(res.data[from]);
+            })
+    }, [from]);
+
+    // การตั้งค่าตัวเลือกสกุลเงินและการคำนวณอัตราแลกเปลี่ยน
+    useEffect(() => {
+        setOptions(Object.keys(info));
+        convert();
+    }, [info])
+
+    // ฟังก์ชันคำนวณอัตราแลกเปลี่ยน 
+    function convert() {
+        var rate = info[to];
+        setOutput(input * rate);
+    }
+
+    // ฟังก์ชันสลับสกุลเงิน
+    function flip() {
+        var temp = from;
+        setFrom(to);
+        setTo(temp);
+    }
+
+    return (
+        <div className="App">
+            <div className="heading">
+                <h1>Currency converter</h1>
+            </div>
+            <div className="container">
+                <div className="left">
+                    <h3>จำนวน</h3>
+                    <input type="text"
+                        placeholder="กรอกจำนวน"
+                        onChange={(e) => setInput(e.target.value)} />
+                </div>
+                <div className="middle">
+                    <h3>จาก</h3>
+                    <Dropdown 
+                        className="react-dropdown"
+                        options={options}
+                        onChange={(e) => { setFrom(e.value) }}
+                        value={from} 
+                        placeholder="From" />
+                </div>
+                <div className="switch">
+                    <HiSwitchHorizontal size="30px"
+                        onClick={() => { flip() }} />
+                </div>
+                <div className="right">
+                    <h3>แปลงเป็น</h3>
+                    <Dropdown 
+                        className="react-dropdown"
+                        options={options}
+                        onChange={(e) => { setTo(e.value) }}
+                        value={to} 
+                        placeholder="To" />
+                </div>
+            </div>
+            <div className="result">
+                <button onClick={() => { convert() }}>แปลง</button>
+                <h2>แปลงเป็น:</h2>
+                <p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
